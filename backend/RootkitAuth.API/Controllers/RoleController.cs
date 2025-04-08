55909f9283c49e6cbc161038498d4babf4b4005c
Grace Farnsworth <gmfarnz@byu.edu>
2025-04-08 10:44:54 -0600

    added some rbac

---
+++ backend/RootkitAuth.API/Controllers/RoleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RootkitAuth.API.Controllers;
+
+public class RoleController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
